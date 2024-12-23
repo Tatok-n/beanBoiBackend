907ffe513ccdba8f4be0f18213c5cfdf733e49f8
Tatok-n <tnhunter2004@gmail.com>
2024-12-23 18:06:52 -0500

    Setup spring project

---
+++ beanBoiBackend/src/main/java/com/beanBoi/beanBoiBackend/beanBoiBackend/BeanBoiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beanBoi.beanBoiBackend.beanBoiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanBoiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanBoiBackendApplication.class, args);
+	}
+
+}
---
+++ beanBoiBackend/src/test/java/com/beanBoi/beanBoiBackend/beanBoiBackend/BeanBoiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanBoi.beanBoiBackend.beanBoiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanBoiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
