e6e0b03fe2cb82806bcd3d68126815fa25a8afd7
Tatok-n <tnhunter2004@gmail.com>
2025-01-31 15:55:16 -0500

    enabled CORS

---
+++ beanBoiBackend/src/main/java/com/beanBoi/beanBoiBackend/beanBoiBackend/WebConfig.java
@@ -0,0 +1,17 @@
+package com.beanBoi.beanBoiBackend.beanBoiBackend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:64761") // Allow requests from your Flutter app
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specific HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials (if needed)
+    }
+}
