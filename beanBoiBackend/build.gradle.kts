361f9817a5942ab00327bc83abb680f98abd5cea
Tatok-n <tnhunter2004@gmail.com>
2026-03-07 21:16:13 -0500

    UpdatedRepositories

--- beanBoiBackend/build.gradle.kts
+++ beanBoiBackend/build.gradle.kts
@@ -1,38 +1,44 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.4.1"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.beanBoi.beanBoiBackend"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(23)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("com.google.firebase:firebase-admin:8.2.0")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.data:spring-data-mongodb:5.1.0-M1")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    // Web starter for REST controllers and web endpoints
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    
+    // MongoDB starter - this provides auto-configuration for MongoTemplate and all Spring Data MongoDB features
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    
+    // Project Lombok for reducing boilerplate code
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    
+    // Test dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- beanBoiBackend/src/main/java/com/beanBoi/beanBoiBackend/beanBoiBackend/core/repositories/BeanRepository.java
+++ beanBoiBackend/src/main/java/com/beanBoi/beanBoiBackend/beanBoiBackend/core/repositories/BeanRepository.java
@@ -1,108 +1,14 @@
 package com.beanBoi.beanBoiBackend.beanBoiBackend.core.repositories;
 
 import com.beanBoi.beanBoiBackend.beanBoiBackend.core.models.Bean;
-import com.beanBoi.beanBoiBackend.beanBoiBackend.core.models.DocumentData;
-import com.beanBoi.beanBoiBackend.beanBoiBackend.firestore.repositories.FirestoreImplementation;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Query;
-import com.google.cloud.firestore.QuerySnapshot;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 
 @Repository
-public class BeanRepository extends DocumentRepository{
-
-
-    private final FirestoreImplementation firestoreImplementation;
-
-    public BeanRepository(FirestoreImplementation firestoreImplementation) {
-        this.collectionName = "beans";
-        this.firestoreImplementation = firestoreImplementation;
-    }
-
-    @Override
-    public Map<String, Object> getAsMap(DocumentData data) {
-        Bean bean = (Bean) data;
-        Map<String, Object> beanMap = new HashMap<>();
-        beanMap.put("name", bean.getName());
-        beanMap.put("process", bean.getProcess());
-        beanMap.put("origin", bean.getOrigin());
-        beanMap.put("uid", bean.getUid());
-        beanMap.put("roaster", bean.getRoaster());
-        beanMap.put("altitude", bean.getAltitude());
-        beanMap.put("price", (bean.getPrice()));
-        beanMap.put("roastDegree", bean.getRoastDegree());
-        beanMap.put("tastingNotes", bean.getTastingNotes());
-        beanMap.put("isActive", bean.isActive());
-        beanMap.put("timesPurchased", bean.getTimesPurchased());
-        beanMap.put("id", bean.getId());
-
-        return beanMap;
-    }
-
-    @Override
-    public DocumentData getFromMap(Map<String, Object> map) {
-        Bean bean = new Bean();
-        bean.setName(map.get("name").toString());
-        bean.setProcess(map.get("process").toString());
-        bean.setOrigin(map.get("origin").toString());
-        bean.setUid(map.get("uid").toString());
-        bean.setRoaster(map.get("roaster").toString());
-        bean.setTastingNotes(map.get("tastingNotes").toString());
-        bean.setAltitude(Long.parseLong(map.get("altitude").toString()));
-        bean.setPrice(Float.parseFloat(map.get("price").toString()));
-        bean.setRoastDegree(Long.parseLong(map.get("roastDegree").toString()));
-        bean.setActive(Boolean.parseBoolean((map.get("isActive").toString())));
-        bean.setTimesPurchased(Integer.parseInt((map.get("timesPurchased").toString())));
-        bean.setId(map.get("id").toString());
-        return bean;
-    }
-
-    @Override
-    public DocumentReference saveDocument(DocumentData data) {
-        if (data.getId() == null) {
-            data.setId(getNewId());
-        }
-        return firestoreImplementation.addDocumentToCollectionWithId(collectionName,getAsMap(data), data.getId());
-    }
-
-    public Bean getBeanById(String beanId) {
-        return (Bean) getDocumentById(beanId);
-    }
-
-
-
-    public List<Bean> getActiveBeansForUser(String uid) {
-        CollectionReference collectionReference = firestoreImplementation.getCollectionReference(collectionName);
-        Query isActive =  collectionReference.whereEqualTo("uid", uid).whereEqualTo("isActive", true);
-        ApiFuture<QuerySnapshot> querySnapshot = isActive.get();
-        try {
-
-            return querySnapshot.get().getDocuments().stream()
-                    .map(queryDocumentSnapshot ->  (Bean) getFromMap(queryDocumentSnapshot.getData()))
-                    .toList();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } catch (ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    public Bean verifyBean(DocumentReference reference) {
-        Bean bean = new Bean();
-        if (firestoreImplementation.getDocumentFromReference(reference).getData() == null) {
-            bean.setName("DELETED");
-        } else {
-            bean = getBeanById(firestoreImplementation.getDocumentFromReference(reference).getId());
-        }
-        return bean;
-    }
+public interface BeanRepository extends MongoRepository<Bean, String> {
+    List<Bean> findAllByUserIdAndActive(Integer userId, Boolean active);
 
 }
